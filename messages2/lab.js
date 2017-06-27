91ed6e83381016c214109cb5183f3f4460a0e5d2
Elia RL <elia.rodriguez.lopez@gmail.com>
2017-06-27 14:36:15 -0400

    updating

--- messages2/lab.js
+++ messages2/lab.js
@@ -1,1 +1,19 @@
-// Your JavaScript goes here...
+
+function parse(){
+	var xml = new XMLHttpRequest();
+	xml.open("GET","data.json",true);
+	xml.onreadystatechange = function(){
+		if (this.readyStat == 4 && this.status == 200){
+			var data = JSON.parse(this.responseText);
+			newData = "";
+			for (i = 0; i < data.length; i++){
+				newData += data[i].content + '<span class="username">' + data[i].username + '</span>' + '<br/>'
+			};
+			document.getElementById('messages').innerHTML = newData;
+		}
+		else {
+			console.log("What's going on?")
+		}
+	};
+	xml.send();
+};
