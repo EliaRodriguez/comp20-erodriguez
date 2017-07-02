16f9b71f87539c2eb460cfc80981bbe7b13ffc68
Elia RL <elia.rodriguez.lopez@gmail.com>
2017-07-02 16:42:54 -0700

    update Lab 8 before I mess everything up again

--- mbta/redline.js
+++ mbta/redline.js
@@ -1,15 +1,4 @@
-var map, mainbranch, fork_one, fork_two, all, southstation, infoWindow, marker; 
-
-// var image = new Image();
-
-
-// image.onload = function(){
-//                   url: 't.png',
-//                   size: new google.maps.Size(25,25),
-//                   origin: new google.maps.Point(0,0),
-//                   anchor: new google.maps.Point(0,25)  
-//                 };
-            
+var map, mainbranch, fork_one, fork_two, all, southstation, infoWindow, marker;             
 
 function initMap(){
   var southstation = new google.maps.LatLng(42.352271, -71.05524200000001);
@@ -18,27 +7,7 @@ function initMap(){
           center: southstation
   }); 
 
-  // var marker = new google.maps.Marker({
-  //   position: southstation,
-  //   map: map
-  // });
-
-
-
-
-
-    //       //   Shapes define the clickable region of the icon. The type defines an HTML
-    //     // <area> element 'poly' which traces out a polygon as a series of X,Y points.
-    //     // The final coordinate closes the poly by connecting to the first coordinate.
-    // var shape = {
-    //       coords: [1, 1, 1, 25, 25, 25, 25, 1],
-    //       type: 'poly'
-    // };
-
-
-
-  // var markers = [];
-
+// List of stations for markers and polylines
   var mainbranch = [{lat:42.395428,lng:-71.142483},{lat:42.39674,lng:-71.121815},{lat:42.3884,lng:-71.11914899999999},{lat:42.373362,lng:-71.118956},{lat:42.365486,lng:-71.103802},{lat:42.36249079,lng:-71.08617653},{lat:42.361166,lng:-71.070628},{lat:42.35639457,lng:-71.0624242},{lat:42.355518,lng:-71.060225},{lat:42.352271,lng:-71.05524200000001},{lat:42.342622,lng:-71.056967},{lat:42.330154,lng:-71.057655},{lat:42.320685,lng:-71.052391}];
   var fork_one = [{lat:42.320685,lng:-71.052391},{lat:42.275275,lng:-71.029583},{lat:42.2665139,lng:-71.0203369},{lat:42.251809,lng:-71.005409},{lat:42.233391,lng:-71.007153},{lat:42.2078543,lng:-71.0011385}];
   var fork_two = [{lat:42.320685,lng:-71.052391},{lat:42.31129,lng:-71.053331},{lat:42.300093,lng:-71.061667},{lat:42.29312583,lng:-71.06573796000001},{lat:42.284652,lng:-71.06448899999999}];
@@ -67,17 +36,7 @@ function initMap(){
   {"id":"Ashmont", "lat":42.284652, "lng":-71.06448899999999}
   ]; 
 
-
-  // for (i = 0; i < ugh.length; i++) {
-  //     var station = new google.maps.LatLng({ugh[i].lat,ugh[i].lng});
-  //     // var name = all[i].id;
-  //     var marker = new google.maps.Marker({
-  //       position: station,
-  //       map: map 
-  //     });
-
-  // };  
-
+// For loop rendering markers on Redline. Working version referenced from example on Github by Professor Ming Chow
   var allMarkers = [];
 
   for (var i = 0; i < all.length; i++) {
@@ -90,7 +49,7 @@ function initMap(){
     marker.setMap(map);
   }
 
-
+// Render polylines mapping Redline. Reference google developers code. 
   var mainbranchPath = new google.maps.Polyline({
     path: mainbranch,
     geodesic: true,
@@ -134,12 +93,6 @@ function initMap(){
       title: "My Location"      
       });
     });
-  
-
-  //   google.maps.event.addListener(marker, 'click', function() {
-  //   infowindow.setContent(stuff);
-  //   infowindow.open(map, marker);
-  // });
       
   }
   else {
