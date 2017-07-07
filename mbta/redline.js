cf38ba1ce888a365401576722fc1be1ac9f44c44
Elia RL <elia.rodriguez.lopez@gmail.com>
2017-07-06 21:15:37 -0700

    get rid of curl

--- mbta/redline.js
+++ mbta/redline.js
@@ -138,17 +138,9 @@ function getMyLocation() {
       var myLat = position.coords.latitude;
       var myLng = position.coords.longitude;
       updateMap(myLat, myLng);
-      // var url = "sample-url.php";
-      // var params = "lorem=ipsum&name=alpha";
-      // var xhr = new XMLHttpRequest();
-      // xhr.open("POST", url, true);
 
-      //Send the proper header information along with the request
-      // xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-
-      // xhr.send(params);
-      curl --data "latitude"=myLat http:"//localhost:3000/"
-      curl --data "longitude"=myLng http:"//localhost:3000/"
+      // curl --data "latitude"=myLat http:"//localhost:3000/userlocation";
+      // curl --data "longitude"=myLng http:"//localhost:3000/userlocation";
     });
  }
  else {
